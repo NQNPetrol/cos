c99298e93eafacccb718238cfa8d7f48a2fddd49
AnalyticsLover <pardiniluciafu@gmail.com>
2025-07-10 20:53:37 -0300

    corrigiendo view seguimientos, agregando index y insertando lista filtrable

--- app/Http/Controllers/SeguimientoController.php
+++ app/Http/Controllers/SeguimientoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Evento;
+use App\Models\Seguimiento;
 use Illuminate\Http\Request;
 
 class SeguimientoController extends Controller
@@ -16,6 +17,36 @@ class SeguimientoController extends Controller
         return view('seguimientos.nuevo', [
             'eventos' => $eventos,
             'header' => 'Nuevo Seguimiento'
+        ]);
+    }
+
+    public function index(Request $request)
+    {
+        $query = Seguimiento::with(['evento', 'user'])
+            ->latest();
+        
+        if ($request->has('estado') && in_array($request->estado, ['ABIERTO', 'EN REVISION', 'CERRADO'])){
+            $query->where('estado', $request->estado);
+        }
+
+        if ($request->has('evento_id')) {
+            $query->where('evento_id', $request->evento_id);
+        }
+
+        if ($request->has('busqueda')) {
+            $query->where(function($q) use ($request){
+                $q->where('titulo', 'like', '%'.$request->busqueda.'%')
+                ->orWhere('descripcion', 'like', '%'.$request->busqueda.'%');
+            });
+        }
+
+        $seguimientos = $query->paginate(10);
+        $eventos = Evento::all();
+
+        return view('seguimientos.index', [
+            'seguimientos' => $seguimientos,
+            'eventos' => $eventos,
+            'filtros' => $request->only(['estado', 'evento_id', 'busqueda'])
         ]);
     }
 }
