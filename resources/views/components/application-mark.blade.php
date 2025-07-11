33d11eb9118b4ea44eb83021f17c886bbcb19309
AnalyticsLover <pardiniluciafu@gmail.com>
2025-07-11 17:28:29 -0300

    cambiando logo principal

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,8 +1,1 @@
-<svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-  <rect width="64" height="64" rx="12" fill="#1e293b"/>
-  <path d="M32 8 L12 20 V36 C12 46 32 56 32 56 C32 56 52 46 52 36 V20 L32 8 Z" 
-        fill="#1f2937" stroke="#bfa25a" stroke-width="2" />
-  <circle cx="32" cy="32" r="8" fill="#0a0f1c" stroke="#bfa25a" stroke-width="2"/>
-  <circle cx="32" cy="32" r="3" fill="#bfa25a" />
-  <path d="M20 24 L44 40 M44 24 L20 40" stroke="#bfa25a" stroke-width="1.5" opacity="0.6"/>
-</svg>
+<img src="{{ asset('COS.png') }}" {{ $attributes }} alt="Logo de la aplicación" class="h-9 w-auto">
