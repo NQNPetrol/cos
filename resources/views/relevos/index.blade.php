daebd8801b141fa907f06b02bb3f610b507b8289
AnalyticsLover <pardiniluciafu@gmail.com>
2025-07-14 18:51:25 -0300

    agregando seccion turnos y editando seccion relevos

--- resources/views/relevos/index.blade.php
+++ resources/views/relevos/index.blade.php
@@ -1,12 +1,3 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h2 class="text-2xl font-semibold">Listado de Relevos</h2>
-                    <!-- Contenido vendrá aquí -->
-                </div>
-            </div>
-        </div>
-    </div>
+
 </x-app-layout>
