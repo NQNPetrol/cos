c5b72f187826b06e2a036e02d77cb25df39a783d
AnalyticsLover <pardiniluciafu@gmail.com>
2025-10-03 16:14:20 -0300

    adaptando vistas para layout clientes y mejorando aspecto

--- resources/views/alertas/flytbase/index.blade.php
+++ resources/views/alertas/flytbase/index.blade.php
@@ -78,7 +78,7 @@
                                 <!-- Botón Enviar -->
                                 <div class="flex justify-end">
                                     <button id="triggerAlarmBtn"
-                                            class="inline-flex items-center px-6 py-3 bg-green-600 border border-transparent rounded-md font-medium text-sm text-white uppercase tracking-widest hover:bg-blue-700 focus:outline-none focus:border-blue-800 focus:ring ring-blue-300 disabled:opacity-25 transition ease-in-out duration-150">
+                                            class="inline-flex items-center px-6 py-3 bg-green-600 border border-transparent rounded-md font-medium text-sm text-white uppercase tracking-widest hover:bg-green-700 focus:outline-none focus:border-cyan-800 focus:ring ring-cyan-300 disabled:opacity-25 transition ease-in-out duration-150">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-exclamation mr-2" viewBox="0 0 16 16">
                                             <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855a.75.75 0 0 0-.124 1.329l4.995 3.178 1.531 2.406a.5.5 0 0 0 .844-.536L6.637 10.07l7.494-7.494-1.895 4.738a.5.5 0 1 0 .928.372zm-2.54 1.183L5.93 9.363 1.591 6.602z"/>
                                             <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1.5a.5.5 0 0 1-1 0V11a.5.5 0 0 1 1 0m0 3a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
