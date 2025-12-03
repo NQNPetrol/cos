ed4543c843929c7fddc4bf00b020006933b2347a
Rodrigo <ing.rsuarez@gmail.com>
2025-12-03 15:12:17 -0300

    Actualizo mapa

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -735,10 +735,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     zoomControl: true
                 });
                 
-                // Agregar capa de tiles (estilo Voyager con colores)
-                L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-                    subdomains: 'abcd',
+                // Agregar capa de tiles (OpenStreetMap estándar - colores claros)
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
                     maxZoom: 19
                 }).addTo(map);
                 
