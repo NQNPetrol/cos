b17f2d440417ef3e74f68b931b9c91bc83406108
Rodrigo <ing.rsuarez@gmail.com>
2025-12-03 14:23:35 -0300

    soluciono layout vite en cliente

--- resources/views/layouts/cliente.blade.php
+++ resources/views/layouts/cliente.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Centro de Operaciones')</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     @livewireStyles
     
