6f832a7ee27c69d3dccb7f0b4e89152bfd1af72f
AnalyticsLover <pardiniluciafu@gmail.com>
2025-12-05 15:25:59 -0300

    corriendo estilos

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
-import EventosBarras from './Components/EventosBarras.vue';
-import ClientDashboard from './Components/Dashboard/ClientDashboard.vue';
-import BarChart from './Components/Dashboard/BarChart.vue';
-import HeatmapChart from './Components/Dashboard/HeatmapChart.vue';
+import EventosBarras from './components/EventosBarras.vue';
+import ClientDashboard from './components/Dashboard/ClientDashboard.vue';
+import BarChart from './components/Dashboard/BarChart.vue';
+import HeatmapChart from './components/Dashboard/HeatmapChart.vue';
 
 console.log('app.js cargando...');
 console.log('Leaflet disponible?', typeof L !== 'undefined' ? 'Sí' : 'No');
