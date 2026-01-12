5b76066c9d26fcdf7c6fce2a77b7075b5d486965
AnalyticsLover <pardiniluciafu@gmail.com>
2026-01-12 16:47:07 -0300

    config vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/css/modern-ui.css', 
+                'resources/js/app.js', 
+                'resources/js/modern-navigation.js', 
+                'resources/js/modern-search.js'
+            ],
             refresh: true,
         }),
         vue(),
